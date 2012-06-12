8ad9cf8ba62ff5257fb826bb448ee2e31674cea4
manfer <manfer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-12 10:00:14 +0000

    Prepared for version 0.8
    git-svn-id: http://plugins.svn.wordpress.org/contact-form-7-bwp-recaptcha-extension/trunk@556589 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- contact-form-7-bwp-recaptcha-extension.php
+++ contact-form-7-bwp-recaptcha-extension.php
@@ -3,7 +3,7 @@
 Plugin Name: Contact Form 7 BWP reCAPTCHA Extension
 Plugin URI: http://www.manfersite.tk/cf7bwpcapt
 Description: Provides Better WordPress reCAPTCHA possibilities to the Contact Form 7 plugin. Requires both.
-Version: 0.7
+Version: 0.8
 Author: Fernando San Julián
 Email: manfer.site@gmail.com
 Author URI: http://www.manfersite.tk
@@ -30,7 +30,7 @@ License: GPL2
 
 // this is the 'driver' file that instantiates the object
 define( 'ALLOW_CF7_BWP_CAPT_INCLUDE', true );
-define( 'CF7BWPRECAPT_VERSION', '0.7' );
+define( 'CF7BWPRECAPT_VERSION', '0.8' );
 define( 'CF7BWPRECAPT_URL', 'http://www.manfersite.tk/cf7bwpcapt' );
 define( 'CF7BWPRECAPT_TITLE', 'Contact Form 7 BWP reCAPTCHA Extension' );
 
